658ee746cb16d0f2199b0cc778dc7255650d2b4f
mdraselmiah95 <russelroy15@gmail.com>
2025-03-06 00:25:59 +0600

    FEAT: client faq hot fix

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,9 +1,9 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminPaths } from "../../routes/admin.routes";
-import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 
-const { Header, Content, Footer, Sider } = Layout;
+import Sidebar from "./Sidebar";
+
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
   return (
@@ -12,34 +12,7 @@ const MainLayout = () => {
         height: "100vh",
       }}
     >
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH Uni</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={sidebarItemsGenerator(adminPaths, "admin")}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -52,9 +25,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Creative Design ©{new Date().getFullYear()} Created by Md.Rasel
-        </Footer>
       </Layout>
     </Layout>
   );
---
+++ client/src/components/layout/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { adminPaths } from "../../routes/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { Layout, Menu } from "antd";
+const { Sider } = Layout;
+
+const Sidebar = () => {
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          color: "white",
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>PH Uni</h1>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItemsGenerator(adminPaths, "admin")}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
