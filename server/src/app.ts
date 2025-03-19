8bfbf44d4b0dcc84662e1beca867c65040d51479
mdraselmiah95 <russelroy15@gmail.com>
2025-03-20 00:27:46 +0600

    FEAT: setup server

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import globalErrorHandler from './app/middlewares/globalErrorhandler';
-import notFound from './app/middlewares/notFound';
-import router from './app/routes';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -15,13 +15,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 // application routes
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi Next Level Developer !');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi Next Level Developer Rasel=>🚀");
 });
 
 app.use(globalErrorHandler);
