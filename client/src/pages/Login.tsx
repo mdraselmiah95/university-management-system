be78c2724898d4e316be9b1e38030cd3c867ee2d
mdraselmiah95 <russelroy15@gmail.com>
2025-03-29 15:24:10 +0600

    FEAT:clinet AUth setup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,28 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" {...register("password")} />
+      </div>
+
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
