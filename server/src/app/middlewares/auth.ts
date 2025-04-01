1a1c923c6722e50a3423052b02b622ecf0400edf
mdraselmiah95 <russelroy15@gmail.com>
2025-04-01 21:33:01 +0600

    FEAT:client Academic semester

--- server/src/app/middlewares/auth.ts
+++ server/src/app/middlewares/auth.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import config from '../config';
-import AppError from '../errors/AppError';
-import { TUserRole } from '../modules/User/user.interface';
-import { User } from '../modules/User/user.model';
-import catchAsync from '../utils/catchAsync';
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import AppError from "../errors/AppError";
+import { TUserRole } from "../modules/User/user.interface";
+import { User } from "../modules/User/user.model";
+import catchAsync from "../utils/catchAsync";
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -13,14 +13,20 @@ const auth = (...requiredRoles: TUserRole[]) => {
 
     // checking if the token is missing
     if (!token) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
     }
 
-    // checking if the given token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
+    let decoded;
+
+    try {
+      // checking if the given token is valid
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string
+      ) as JwtPayload;
+    } catch (error) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "Unauthorized");
+    }
 
     const { role, userId, iat } = decoded;
 
@@ -28,37 +34,37 @@ const auth = (...requiredRoles: TUserRole[]) => {
     const user = await User.isUserExistsByCustomId(userId);
 
     if (!user) {
-      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+      throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
     }
     // checking if the user is already deleted
 
     const isDeleted = user?.isDeleted;
 
     if (isDeleted) {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+      throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !");
     }
 
     // checking if the user is blocked
     const userStatus = user?.status;
 
-    if (userStatus === 'blocked') {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
+    if (userStatus === "blocked") {
+      throw new AppError(httpStatus.FORBIDDEN, "This user is blocked ! !");
     }
 
     if (
       user.passwordChangedAt &&
       User.isJWTIssuedBeforePasswordChanged(
         user.passwordChangedAt,
-        iat as number,
+        iat as number
       )
     ) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized !");
     }
 
     if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
-        'You are not authorized  hi!',
+        "You are not authorized  hi!"
       );
     }
 
