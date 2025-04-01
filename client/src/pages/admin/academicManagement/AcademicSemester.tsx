bccdd577c5d111526f0a88294b283aa305959a25
mdraselmiah95 <russelroy15@gmail.com>
2025-04-01 18:57:37 +0600

    FEAT:client Academic semester

--- client/src/pages/admin/academicManagement/AcademicSemester.tsx
+++ client/src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,9 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+
 const AcademicSemester = () => {
+  const { data } = useGetAllSemestersQuery(undefined);
+  console.log({ data });
+
   return <div>AcademicSemester</div>;
 };
 
