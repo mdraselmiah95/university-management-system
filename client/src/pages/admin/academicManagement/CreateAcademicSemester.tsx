c39f9ba4cd01031a15cbd03bfde6dc412d5520cd
mdraselmiah95 <russelroy15@gmail.com>
2025-04-09 23:58:42 +0600

    FEAT: Create semester and other hot fix

--- client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -22,8 +22,8 @@ const CreateAcademicSemester = () => {
   };
 
   const academicSemesterSchema = z.object({
-    name: z.string({ required_error: "Name is required" }),
-    year: z.string({ required_error: "Year is required" }),
+    name: z.string({ required_error: "Please select a Name" }),
+    year: z.string({ required_error: "Please select a Year" }),
     startMonth: z.string({ required_error: "Start Month is required" }),
     endMonth: z.string({ required_error: "End Month is required" }),
   });
