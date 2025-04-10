a03ee0fdbf9b4ef8a3467302cfad82d904d49290
mdraselmiah95 <russelroy15@gmail.com>
2025-04-11 00:32:04 +0600

    feat: University management semester

--- client/src/pages/admin/academicManagement/AcademicSemester.tsx
+++ client/src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,10 +1,97 @@
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
+import { Table } from "antd";
+import type { TableColumnsType, TableProps } from "antd";
+import { useState } from "react";
+
+type TableRowSelection<T extends object = object> =
+  TableProps<T>["rowSelection"];
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
 
 const AcademicSemester = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const { data } = useGetAllSemestersQuery(undefined);
   console.log({ data });
 
-  return <div>AcademicSemester</div>;
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+    },
+  ];
+
+  const dataSource = Array.from({ length: 46 }).map<DataType>((_, i) => ({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  }));
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection: TableRowSelection<DataType> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: "odd",
+        text: "Select Odd Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: "even",
+        text: "Select Even Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Table<DataType>
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={dataSource}
+      />
+    </div>
+  );
 };
 
 export default AcademicSemester;
