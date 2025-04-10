a92020c98acf691e36b559665e7e93ae7b38dceb
mdraselmiah95 <russelroy15@gmail.com>
2025-04-11 00:41:15 +0600

    feat: University management semester

--- client/src/pages/admin/academicManagement/AcademicSemester.tsx
+++ client/src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -46,8 +46,6 @@ const AcademicSemester = () => {
           ],
         },
       ],
-      // specify the condition of filtering result
-      // here is that finding the name started with `value`
       onFilter: (value, record) => record.name.indexOf(value as string) === 0,
       sorter: (a, b) => a.name.length - b.name.length,
       sortDirections: ["descend"],
