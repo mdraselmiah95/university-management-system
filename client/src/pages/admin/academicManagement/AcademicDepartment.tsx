5ef74b833eaf061f873fa43d20746096f299d8a7
mdraselmiah95 <russelroy15@gmail.com>
2025-04-13 00:40:34 +0600

    FEAT: Academic Department

--- client/src/pages/admin/academicManagement/AcademicDepartment.tsx
+++ client/src/pages/admin/academicManagement/AcademicDepartment.tsx
@@ -1,4 +1,7 @@
 import { useGetAcademicDepartmentsQuery } from "../../../redux/features/admin/academicManagement.api";
+import { Button, Table } from "antd";
+import type { TableColumnsType, TableProps } from "antd";
+import { TTableData } from "./AcademicFaculty";
 
 const AcademicDepartment = () => {
   const {
@@ -9,7 +12,55 @@ const AcademicDepartment = () => {
 
   console.log({ DepartmentData });
 
-  return <div>AcademicDepartment</div>;
+  const tableData = DepartmentData?.data?.map(({ _id, name }) => ({
+    key: _id,
+    name,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Department Name",
+      dataIndex: "name",
+      key: "name",
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: () => {
+        return (
+          <div>
+            <Button>Action</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  if (isLoading) {
+    return <p>Loading..</p>;
+  }
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    console.log(filters, extra);
+  };
+
+  return (
+    <div>
+      <Table
+        loading={isFetching}
+        columns={columns}
+        dataSource={tableData}
+        onChange={onChange}
+        showSorterTooltip={{ target: "sorter-icon" }}
+      />
+    </div>
+  );
 };
 
 export default AcademicDepartment;
