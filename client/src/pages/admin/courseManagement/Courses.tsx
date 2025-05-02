7ed1e5ad17771142814281ec3cd54305bcac005e
mdraselmiah95 <russelroy15@gmail.com>
2025-05-03 00:20:20 +0600

    Course management

--- client/src/pages/admin/courseManagement/Courses.tsx
+++ client/src/pages/admin/courseManagement/Courses.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagement.api";
 import { Button, Modal, Table } from "antd";
+import { useGetAllFacultiesQuery } from "../../../redux/features/admin/userManagement.api";
 
 const Courses = () => {
   const {
@@ -32,8 +33,8 @@ const Courses = () => {
     {
       title: "Action",
       key: "action",
-      render: () => {
-        return <AddFacultyModal />;
+      render: (items) => {
+        return <AddFacultyModal data={items} />;
       },
     },
   ];
@@ -54,9 +55,10 @@ const Courses = () => {
   );
 };
 
-const AddFacultyModal = () => {
+const AddFacultyModal = ({ data }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const { data: facultyData } = useGetAllFacultiesQuery(undefined);
+  console.log({ facultyData });
   const showModal = () => {
     setIsModalOpen(true);
   };
