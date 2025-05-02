15822ec6f2be8aa8c9c197e974eed9df0ba73051
mdraselmiah95 <russelroy15@gmail.com>
2025-05-02 23:49:34 +0600

    Course management

--- client/src/pages/admin/courseManagement/Courses.tsx
+++ client/src/pages/admin/courseManagement/Courses.tsx
@@ -1,10 +1,63 @@
+import { TableColumnsType } from "antd";
 import { useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagement.api";
+import type { TableProps } from "antd";
+import { Button, Table } from "antd";
 
 const Courses = () => {
-  const { data: courseData } = useGetAllCoursesQuery(undefined);
+  const {
+    data: courseData,
+    isFetching,
+    isLoading,
+  } = useGetAllCoursesQuery(undefined);
   console.log({ courseData });
 
-  return <div>courses</div>;
+  const tableData = courseData?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    prefix,
+    code: `${prefix} ${code}`,
+  }));
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+
+    {
+      title: "Code",
+      dataIndex: "code",
+      key: "code",
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: () => {
+        return (
+          <div>
+            <Button>Assign Faculty</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  if (isLoading) {
+    return <p>Loading..</p>;
+  }
+
+  return (
+    <div>
+      <Table
+        loading={isFetching}
+        columns={columns}
+        dataSource={tableData}
+        showSorterTooltip={{ target: "sorter-icon" }}
+      />
+    </div>
+  );
 };
 
 export default Courses;
