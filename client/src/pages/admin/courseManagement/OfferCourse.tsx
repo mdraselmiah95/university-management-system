e6fca0bd5ffbe69eae776b5223e5bb4052e60c2b
mdraselmiah95 <russelroy15@gmail.com>
2025-06-22 21:40:23 +0600

    FEAT: Offer course added

--- client/src/pages/admin/courseManagement/OfferCourse.tsx
+++ client/src/pages/admin/courseManagement/OfferCourse.tsx
@@ -1,5 +1,88 @@
+import { Button, Card } from "antd";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
+const offerCourseData = {
+  semesterRegistration: "65b6185f13c0a33cdf61589a",
+  academicFaculty: "65b00f3510b74fcbd7a25d86",
+  academicDepartment: "65b00fb010b74fcbd7a25d8e",
+  course: "65b6001fd6ffdd9bfc058329",
+  faculty: "65b0844ccb87974826d0b7af",
+  section: 1,
+  maxCapacity: 30,
+  days: ["Mon", "Wed"],
+  startTime: "12:30",
+  endTime: "14:00",
+};
+
 const OfferCourse = () => {
-  return <div>OfferCourse</div>;
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log("Form submitted with data:", data);
+  };
+
+  return (
+    <div>
+      <Card
+        title="Semester Registration"
+        style={{
+          width: "auto",
+          maxWidth: "400px",
+          margin: "auto",
+          marginTop: "5%",
+          padding: "12px 18px",
+        }}
+      >
+        <PHForm
+          onSubmit={onSubmit}
+          // resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHSelect
+            label="Academic Semester"
+            name="academicSemester"
+            placeholder="Select academic semester"
+            disabled={academicSemesterLoading}
+            options={academicSemestersOptions}
+          />
+
+          <PHSelect
+            label="Academic Status"
+            name="status"
+            placeholder="Select status"
+            options={semesterStatusOptions}
+          />
+
+          <PHDatePicker
+            name="startDate"
+            label="Start Date"
+            placeholder="Select start date"
+          />
+
+          <PHDatePicker
+            name="endDate"
+            label="End Date"
+            placeholder="Select end date"
+          />
+
+          <PHInput
+            type="text"
+            name="minCredit"
+            label="Min Credit"
+            placeholder="Min credit"
+          />
+
+          <PHInput
+            type="text"
+            name="maxCredit"
+            label="Max Credit"
+            placeholder="Max credit"
+          />
+
+          <Button htmlType="submit" type="primary">
+            Submit
+          </Button>
+        </PHForm>
+      </Card>
+    </div>
+  );
 };
 
 export default OfferCourse;
