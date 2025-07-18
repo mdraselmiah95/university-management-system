5996d9f9b6764bcd010f6b2b04dc19aea80c9b81
mdraselmiah95 <russelroy15@gmail.com>
2025-07-18 18:18:46 +0600

    FEAT: Route protection and management

--- client/src/components/layout/ProtectedRoute.tsx
+++ client/src/components/layout/ProtectedRoute.tsx
@@ -1,17 +1,28 @@
 import { ReactNode } from "react";
 import { useAppSelector } from "../../redux/hooks";
-import { useCurrentToken } from "../../redux/features/auth/authSlice";
+import {
+  logout,
+  selectCurrentUser,
+  useCurrentToken,
+} from "../../redux/features/auth/authSlice";
 import { Navigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 type TProtectedRoute = {
   children: ReactNode;
-  role: string;
+  role: string | undefined;
 };
 
 const ProtectedRoute = ({ children, role }: TProtectedRoute) => {
   const token = useAppSelector(useCurrentToken);
+  const userRole = useAppSelector(selectCurrentUser);
 
-  console.log({ role });
+  const dispatch = useDispatch();
+
+  if (role !== undefined && role !== userRole?.role) {
+    dispatch(logout());
+    return <Navigate to="/login" replace={true} />;
+  }
 
   if (!token) {
     return <Navigate to="/login" replace={true} />;
